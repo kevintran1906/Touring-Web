2fe026415c1633107615853eff6da03befa754bc
Kevin Tran <kevintran19062007@gmail.com>
2024-12-17 09:35:43 -0500

    first commit

---
+++ Touring-Website/src/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app >
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+  import DefaultBar from './AppBar.vue'
+  import DefaultView from './View.vue'
+  //
+</script>
