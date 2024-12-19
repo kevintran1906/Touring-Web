f5029afb46e64ef9c2c9ad84166504996e438be5
Kevin Tran <kevintran19062007@gmail.com>
2024-12-19 10:04:10 -0500

    add tours bar

--- Touring-Website/src/components/Navbar.vue
+++ Touring-Website/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app-bar height="80" color="FEF5F8" scroll-behavior="elevate" id="inspire">
-        <v-img src="./earth.png" class=""></v-img>
+        <v-img src="/earth.png" class=""></v-img>
         <v-app-bar-title class ="text-h4">Explore</v-app-bar-title>
         <v-spacer></v-spacer>
         <v-menu transition="slide-x-transition">
@@ -16,11 +16,30 @@
             </v-list>
         </v-menu>
         
+        
         <v-menu transition="slide-x-transition">
+            <template v-slot:activator="{props}">
+            <v-btn color="#6347F9" v-bind="props">Tours +</v-btn>
+
+            </template>
+            <v-list>
+                <v-list-item>
+                    <v-list-item-title>Tour Grid</v-list-item-title>
+                    <v-list-item-title>Tour list</v-list-item-title>
+                    <v-list-item-title>Tour Single</v-list-item-title>
+                </v-list-item>
+            </v-list>
 
         </v-menu>
+    
+
+
+
     </v-app-bar>
 </template>
+
+
+
 
 <script>
 export default {
